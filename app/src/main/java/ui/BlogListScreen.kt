73559f94eb4477e260bb9021256ee5642a6663c5
Shivangi <shivangit1111@gmail.com>
2024-06-09 13:54:10 +0530

    "style: add app title"

--- app/src/main/java/ui/BlogListScreen.kt
+++ app/src/main/java/ui/BlogListScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
@@ -12,7 +13,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import model.BlogPostItem
@@ -22,17 +26,36 @@ import viewmodel.BlogViewModel
 fun BlogListScreen(navController: NavHostController, viewModel: BlogViewModel) {
     val blogPosts by viewModel.blogPosts.collectAsState()
 
-    LazyColumn {
-        items(blogPosts) { blog ->
-            BlogListItem(blog) {
-                navController.navigate("blogDetail/${blog.id}")
+    Column {
+        Text(
+            text = "Blog Reading App",
+            color = Color.Blue,
+            fontFamily = FontFamily.Monospace,
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .wrapContentWidth(Alignment.CenterHorizontally)
+
+        )
+        LazyColumn {
+            items(blogPosts) { blog ->
+                BlogListItem(blog) {
+                    navController.navigate("blogDetail/${blog.id}")
+                }
             }
         }
     }
 }
+
+
+
 @Composable
 fun BlogListItem(blog: BlogPostItem, onClick: () -> Unit) {
-    Card (modifier = Modifier.padding(5.dp)){
+
+
+    Card (modifier = Modifier.padding(8.dp)){
+
         Column(
             modifier = Modifier
                 .fillMaxWidth()
