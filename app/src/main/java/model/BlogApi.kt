fb581ad81ad7ae5b4f3d8c96858c940cc4717dfa
Shivangi <shivangit1111@gmail.com>
2024-06-09 10:36:53 +0530

    "create api interface"

--- app/src/main/java/model/BlogApi.kt
+++ app/src/main/java/model/BlogApi.kt
@@ -1,12 +1,13 @@
 package model
 
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface BlogApi {
 
 
    @GET ("wp-json/wp/v2/posts?per_page=10&page=1")
-   suspend fun getBlogPosts(): List<>
+   suspend fun getBlogPosts(): Response<List<BlogPostItem>>
 
 
 }
