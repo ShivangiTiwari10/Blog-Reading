badbc6834f5410bac46261445eb510e4a9a6eec1
Shivangi <shivangit1111@gmail.com>
2024-06-09 21:42:38 +0530

    feat: implement Pagination

--- app/src/main/java/model/BlogApi.kt
+++ app/src/main/java/model/BlogApi.kt
@@ -2,12 +2,13 @@ package model
 
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
+
 
 interface BlogApi {
-
-
-   @GET ("wp-json/wp/v2/posts?per_page=10&page=1")
-   suspend fun getBlogPosts(): Response<List<BlogPostItem>>
-
-
+   @GET("wp-json/wp/v2/posts")
+   suspend fun getBlogPosts(
+      @Query("per_page") perPage: Int = 10,
+      @Query("page") page: Int
+   ): Response<List<BlogPostItem>>
 }
